6fa19e2f4a42f4873651de8776f4164b787a4d11
MBURU-25 <njerimburu07@gmail.com>
2023-01-06 04:21:45 -0800

    delete at

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    '''Finds all multiples of 2 in a list'''
-    multiples = []
-    for index in range(len(my_list)):
-        if my_list[index] % 2 == 0:
-            multiples.append(True)
-        else:
-            multiples.append(False)
-
-    return (multiples)
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        del my_list[idx]
+        return my_list
